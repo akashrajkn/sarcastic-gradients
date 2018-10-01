d845813e2e1a83d1f6b485d08a38d82fc463854e
akash raj <akashrajkn@gmail.com>
2018-10-01 18:09:53 +0200

    script

--- rnn/part1/rnn_script.sh
+++ rnn/part1/rnn_script.sh
@@ -7,16 +7,16 @@ echo "Start simulations"
 
 echo "-------------------------------------------------------------"
 echo "1 - input_length:5"
-python train.py --input_length=10 --model_type='RNN' --input_dim=10 --train_steps=3000 --learning_rate=0.001
+python train.py --input_length=10 --model_type='RNN' --train_steps=3000 --learning_rate=0.001
 
 echo "-------------------------------------------------------------"
 echo "1 - input_length:6"
-python train.py --input_length=10 --model_type='RNN' --input_dim=10 --train_steps=3000 --learning_rate=0.01
+python train.py --input_length=10 --model_type='RNN' --train_steps=3000 --learning_rate=0.01
 
 echo "-------------------------------------------------------------"
 echo "1 - input_length:7"
-python train.py --input_length=10 --model_type='RNN' --input_dim=10 --train_steps=3000 --learning_rate=0.025
+python train.py --input_length=10 --model_type='RNN' --train_steps=3000 --learning_rate=0.025
 
 echo "-------------------------------------------------------------"
 echo "1 - input_length:8"
-python train.py --input_length=10 --model_type='RNN' --input_dim=10 --train_steps=3000 --learning_rate=0.0001
+python train.py --input_length=10 --model_type='RNN' --train_steps=3000 --learning_rate=0.0001
